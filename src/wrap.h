338450fc33a1538b0bb2d816b62d2f4e5aad8091
KJM <kjm99d@naver.com>
2022-05-06 18:22:02 +0900

    Project Initialize

---
+++ src/wrap.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#ifdef _WIN32
+	#ifdef _DLL
+		#define DLL_EXPORT __declspec(dllexport) 
+	#else
+		#define DLL_EXPORT __declspec(dllimport) 
+	#endif // ifdef _DLL
+
+#else // #ifndef WIN32
+		#define DLL_EXPORT 
+#endif
+
